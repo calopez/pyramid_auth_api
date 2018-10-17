9d82d257cc64251b0865d880e34b91058d2778e6
Carlos Andres Lopez <karloslopez@me.com>
2018-10-17 09:07:26 -0500

    Adapt pyramid-cookiecutter-alchemy scaffold
    Use the files generated by pyramid-cookiecutter-alchemy
    and adapt to the current proyect structure
    
    To run pyramid commands it is necessary to prepend the PYTHONPATH
    PYTHONPATH=./src

--- setup.py
+++ setup.py
@@ -21,4 +21,13 @@ except VersionConflict:
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(use_pyscaffold=True,
+          entry_points={
+              'paste.app_factory': [
+                  'main = tm:main',
+              ],
+              'console_scripts': [
+                  'initialize_tm_db = tm.scripts.initializedb:main',
+              ],
+          },
+          )
