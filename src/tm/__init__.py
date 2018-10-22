d8dc7f801e3e8ebf912a02b098fecd1b369f4850
Carlos Andres Lopez <karloslopez@me.com>
2018-10-22 11:36:31 -0500

    Ability to use env variables in ini files
    + Use a secret_file.ini for secrets
    + Ability to read env vars from secret files.

--- src/tm/__init__.py
+++ src/tm/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+import typing as t
+
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
@@ -17,9 +20,32 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+
+    config = Configurator(settings=expandvars_dict(settings))
+    config.include('.conf.secrets')
+
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
+
+
+def expandvars_dict(settings: dict) -> dict:
+    """Expand all environment variables in a settings dictionary.
+
+    ref: http://stackoverflow.com/a/16446566
+    :returns: Dictionary with settings
+    """
+    return {key: _expandvars(value) for key, value in settings.items()}
+
+
+def _expandvars(value: t.Any) -> t.Any:
+    processed = value
+    if isinstance(value, dict):
+        processed = expandvars_dict(value)
+    elif isinstance(value, (str, bytes)):
+        processed = os.path.expandvars(value)
+    return processed
+
+
