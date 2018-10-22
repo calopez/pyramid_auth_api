74b81544d01d14f8102629b61a9d6b165e204c1e
Carlos Andres Lopez <karloslopez@me.com>
2018-10-22 11:47:29 -0500

    Test secrets file reading

---
+++ tests/system/utils/test_secrets.py
@@ -0,0 +1,17 @@
+"""Test secrets reader."""
+# Standard Library
+import os
+
+from tm.system.utils import secrets
+
+
+def test_read_local_secrets():
+    """Read file referred by a relative path."""
+    data = secrets.read_ini_secrets("conf/test-secrets.ini", strict=False)
+    assert data.get("authentication.secret") == "xxx"
+
+
+def test_file_secrets():
+    """Read file referred by absolute path."""
+    data = secrets.read_ini_secrets("file://" + os.path.abspath("conf/test-secrets.ini"), strict=False)
+    assert data.get("authentication.secret") == "xxx"
